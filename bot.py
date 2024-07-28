db3ceb01bf7c2874031f95b895d98039caa99e14
Nikita007 <nik4ok@gmail.com>
2024-07-28 23:02:40 +0300

    version 1.1

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from aiogram import Bot, Dispatcher
 from config import TOKEN_API
 from Handlers import actions
 from Handlers import ikb_functions
-from Handlers import SHOW
 #чтобы хранить данные в оперативной памяти
 from aiogram.fsm.storage.memory import MemoryStorage
 
